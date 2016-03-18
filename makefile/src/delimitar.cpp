5225cfa9ce05609e3f32392bca986814e37b6dbf
jmml <jmmartinluque@gmail.com>
2016-03-18 19:15:25 +0100

    hecho algunos casos de delimitar

--- makefile/src/delimitar.cpp
+++ makefile/src/delimitar.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <iostream>
-#include "punto.h"
+
+#include "rectangulo.h"
+
 using namespace std;
 
 void Avanzar(std::istream& is) {
@@ -12,8 +14,9 @@ void Avanzar(std::istream& is) {
 }
 
 
-double Longitud(istream &is) {
-  double l = 0;
+Rectangulo RectanguloDelimitador(istream &is) {
+
+  Rectangulo r;
   Punto p1, p2;
 
   Avanzar(is);
@@ -21,19 +24,39 @@ double Longitud(istream &is) {
   Avanzar(is);
 
   while (Leer(is, p2)) { // Mientras tenga éxito la lectura
-    l += Distancia(p1, p2);
+
+    // Si el punto está en el cuadrante inferior respecto de la esquina inferior
+    // izquierda, se cambia la esquina a ese punto.
+    // Si el punto está en el cuadrante superior respecto de la esquina superior
+    // derecha, se cambia la esquina a ese punto
+
+    if (p2.x < r.esq_inf_izq.x) {
+      r.esq_inf_izq.x = p2.x;
+    } else if (p2.x > r.esq_sup_der.x) {
+      r.esq_sup_der.x = p2.x;
+    }
+
+    if (p2.y < r.esq_inf_izq.y) {
+      r.esq_inf_izq.y = p2.y;
+    } else if (p2.y > r.esq_sup_der.y) {
+      r.esq_sup_der.y = p2.y;
+    }
+
+    InicializarRectangulo(r, p1, p2);
     Avanzar(is); // Descarta comentarios y para en siguiente dato
-    p1 = p2;
+
   }
 
-  return l;
+  return r;
 }
 
 int main(int argc, char* argv[]) {
-  double longitud = 0;
+
+  Rectangulo mi_rectangulo;
   bool fin_entrada;
+
   if (argc == 1) { // Si no hemos dado parámetros en la línea de órdenes
-    longitud = Longitud(cin);
+    mi_rectangulo = RectanguloDelimitador(cin);
     fin_entrada = cin.eof();
   }
   else {
@@ -42,7 +65,7 @@ int main(int argc, char* argv[]) {
       cerr << "Error: no se abre " << argv[1] << endl;
       return 1;
     }
-    longitud = Longitud(f);
+    mi_rectangulo = RectanguloDelimitador(f);
     fin_entrada = f.eof();
   }
 
@@ -50,5 +73,9 @@ int main(int argc, char* argv[]) {
      cerr << "Error inesperado. No se ha leído toda la entrada" << endl;
      return 1;
   }
-  cout << "Longitud de los puntos: " << longitud << endl;
+
+  Escribir(cout, mi_rectangulo.esq_inf_izq);
+  Escribir(cout, mi_rectangulo.esq_sup_der);
+  cout << endl;
+
 }
