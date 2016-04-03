6038960c7e96aa288ec8145fb49f9f9fe6100577
jojelupipa <jesus_38b@hotmail.com>
2016-04-03 14:50:28 +0200

    El algoritmo para hacer Ocultar está escrito aprox. pero he violado al segmento del core generado :(

--- esteganografía/src/Prueba modificar bytes.cpp
+++ esteganografía/src/Prueba modificar bytes.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main () {
 
-  unsigned char vector[50] = {'P'}, mensaje[4];
+  unsigned char vectorcillo[50] = {'P'};
+  char mensaje[4];
   cin.getline(mensaje,4);
 
   cout << "Mostramos el vector \"Imagen\": ";
 
-  for (int i = 0; i < 50; i++)
-    cout << vector[i];
+  for (int i = 0; i < 49; i++)
+    cout << vectorcillo[i];
 
     /** IDEA: Suponiendo que el uno se representa como 00000001 tenemos que consultar cada bit de nuestro mensaje
       *       e introducirlo en el de la información de la imagen, nuestro mensaje tiene 8 bits por caracter, por lo
@@ -30,32 +31,57 @@ int main () {
       *
       */
 
-  unsigned char uno, a_cifrar, bit_pos_j, aux;
+  unsigned char uno = 1, a_cifrar, bit_pos_j;
+                                            //  VERY IMPORTANT
+  for (int i = 0; i < 3; i++ ){             // Aquí hay que modificar una precondición que vea cual tiene más tamaño, si
+                                            // el mensaje * 8 bits o la capacidad del vector imagen en sí
 
-  for (int i = 0; i < 4; i++ ){
 
     a_cifrar = mensaje[i];                    //                                      XYYYYYYY
-                                              // uno << 7-0   --->                  & 10000000
-                                              //                                      _________ uno >> 7-0
-      for (int j = 0; i < 7; j++) {           // 00000001     --->   10000000 --->  = X0000000 -----> 0000000X = 1 ó 0 
+                                              //                                    & 10000000
+                                              //         uno << 7-0          --->    _________ uno >> 7-0
+      for (int j = 0; i < 7; j++) {           // 00000001     --->   10000000       = X0000000 -----> 0000000X = 1 ó 0
                                               //                                                      segun el valor de X
-      uno = 1;
-      bit_pos_j =  (uno << (7-j))
+
+      bit_pos_j =  ((uno << (7-j)) & mensaje[i]) >> (7-j);
+
+
+
+
+// Distinción de casos segun el valor de ese bit
+
+      if (bit_pos_j == 1)                                          // "XXXXXXXX OR 00000001 = XXXXXXX1"
+
+        vectorcillo[i * 8 + j] = vectorcillo[i * 8 + j] | uno;
+
+      else if (bit_pos_j == 0) {                                   //11111110 AND XXXXXXXX = XXXXXXX0
+
+        vectorcillo[i * 8 + j] = vectorcillo[i * 8 + j] & ~uno;
+      }
+
+      else
+
+        cout << vectorcillo[i * 8 + j] << "\tAlgo has hecho mal pillín";
+
     }
-
-
   }
 
+  cout << "El vectorcillo (nuestra imagen) ahora es: ";
+
+  for (int i = 0; i < 49; i++)
+    cout << vectorcillo[i] << " ";
+
+  cout << "\n\nY el mensaje: ";
+
+  for (int i = 0; i < 3;i++)
+    cout << mensaje[i];
 
 
-  wtf = byte | cero;
-  dos = byte & cero;
+  // A PARTIR DE AQUÍ COMIENZA EL CÓMO DESCIFRAR EL MENSAJE
 
 
-  cout << byte << endl;
 
-cout << wtf << endl;
-cout << dos << endl;
+//  IGNORE ALL THE WAY DOWN PLZ
 
 
 }
