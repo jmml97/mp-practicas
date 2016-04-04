48ef39dd4c1e11cead23a36b3b8fc6c5355d8746
jmml <jmmartinluque@gmail.com>
2016-04-04 21:28:08 +0200

    Limpiado el proyecto (tengo que recordar hacer mrproper antes de hacer commit).

--- esteganografía/doc/html/codificar_8h.js
+++
@@ -1,5 +0,0 @@
-var codificar_8h =
-[
-    [ "Ocultar", "codificar_8h.html#a11e8af8a8ceec2c65d89b64e63949fbe", null ],
-    [ "Revelar", "codificar_8h.html#a34a85ae6b7b820acddda3e27a8fcb968", null ]
-];
