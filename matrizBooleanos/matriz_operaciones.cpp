9e388a134c58c6cfebc79a4c01d931cdad266d4e
jojelupipa <jesus_38b@hotmail.com>
2016-04-11 19:24:52 +0200

    Arreglada función Leer mediante flujo, cambiada "la sintaxis de Class por la de Struct"

--- matrizBooleanos/matriz_operaciones.cpp
+++ matrizBooleanos/matriz_operaciones.cpp
@@ -4,26 +4,40 @@
 #include "matriz_bit.h"
 
 
-bool Leer(std::istream& is, MatrizBit& m) {
+bool Leer(std::istream& is, MatrizBit& m) {   // Estamos leyendo m, por lo cual no está inicializada
+                                              // leemos dos valores y llamamos a inicializar, además Filas no es
+                                              // un método de una clase matriz, sino una función global, tenemos
+                                              // que usar Filas (m), eso se aplica a todo lo demás
+  int fils, colms;
+  is >> fils >> colms;
 
-  for (int i = 0; i < m.Filas(); i++) {
-    for (int j = 0; j < m.Columnas(); j++) {
-      char v
-      is >> v;
-      if (v == '1')
-        Set(m, i, j, true);
-      else
-        Set(m, i, j, false);
+  if (Inicializar(m,fils,colms)){
 
+
+    for (int i = 0; i < Filas(m); i++) {
+      for (int j = 0; j < Columnas(m); j++) {
+        char v;
+        is >> v;
+        if (v == '1')
+          Set(m, i, j, true);
+        else if (v == '0')
+          Set(m, i, j, false);
+        else
+          return false;
+
+        }
     }
+    return true;
   }
 
+  else
+    return false;
 }
 
 bool Escribir(std::ostream& os, const MatrizBit& m) {
 
-  for (int i = 0; i < m.Filas(); i++) {
-    for (int j = 0; j < m.Columnas(); j++) {
+  for (int i = 0; i < Filas(m); i++) {
+    for (int j = 0; j < Columnas(m); j++) {
       bool v = Get(m, i, j);
       if (v == true)
         os << '1';
@@ -34,8 +48,10 @@ bool Escribir(std::ostream& os, const MatrizBit& m) {
 
 }
 
-bool Leer(const char nombre[], MatrizBit& m) {
+bool Leer(const char nombre[], MatrizBit& m) {    // Tenemos  que crear un flujo para que al pasarle el nombre del archivo
+                                                  // repita el  mismo proceso que antes
 
+  std::istream f;
 
 
 }
@@ -50,10 +66,10 @@ void And(MatrizBit& res, const MatrizBit& m1, const MatrizBit& m2) {
 
   bool elemento;
 
-  if ((m1.Filas() == m2.Filas()) && (m1.Columnas() == m2.Columnas())) {
+  if ((Filas(m1) == Filas(m2)) && (Columnas(m1) == Columnas(m2))) {
 
-    for (int i = 0; i < m.Filas(); i++) {
-      for (int j = 0; j < m.Columnas(); j++) {
+    for (int i = 0; i < Filas(m); i++) {
+      for (int j = 0; j < Columnas(m); j++) {
         elemento = Get(m1, i, j) && Get(m2, i, j);
         Set(res, i, j, elemento);
 
@@ -70,10 +86,10 @@ void Or(MatrizBit& res, const MatrizBit& m1, const MatrizBit& m2) {
 
   bool elemento;
 
-  if ((m1.Filas() == m2.Filas()) && (m1.Columnas() == m2.Columnas())) {
+  if ((Filas(m1) == Filas(m2)) && (Columnas(m1) == Columnas(m2))) {
 
-    for (int i = 0; i < m.Filas(); i++) {
-      for (int j = 0; j < m.Columnas(); j++) {
+    for (int i = 0; i < Filas(m); i++) {
+      for (int j = 0; j < Columnas(m); j++) {
         elemento = Get(m1, i, j) || Get(m2, i, j);
         Set(res, i, j, elemento);
 
@@ -90,8 +106,8 @@ void Or(MatrizBit& res, const MatrizBit& m1, const MatrizBit& m2) {
 
 void Not(MatrizBit& res, const MatrizBit& m) {
 
-  for (int i = 0; i < m.Filas(); i++) {
-    for (int j = 0; j < m.Columnas(); j++) {
+  for (int i = 0; i < Filas(m); i++) {
+    for (int j = 0; j < Columnas(m); j++) {
       elemento = !Get(m1, i, j);
       Set(res, i, j, elemento);
 
@@ -102,8 +118,8 @@ void Not(MatrizBit& res, const MatrizBit& m) {
 
 void Traspuesta(MatrizBit& res, const MatrizBit& m) {
 
-  for (int i = 0; i < m.Filas(); i++) {
-    for (int j = 0; j < m.Columnas(); j++) {
+  for (int i = 0; i < Filas(m); i++) {
+    for (int j = 0; j < Columnas(m); j++) {
       elemento = Get(m1, i, j);
       Set(res, j, i, elemento);
 
