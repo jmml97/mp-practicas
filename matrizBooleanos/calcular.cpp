49b585dd3f13ecf72737823437d71c55b29b180a
jmml <jmmartinluque@gmail.com>
2016-04-12 11:45:02 +0200

    mejorado el estilo de los strcmp

--- matrizBooleanos/calcular.cpp
+++ matrizBooleanos/calcular.cpp
@@ -9,15 +9,14 @@ using namespace std;
 int main(int argc, char* argv[]) {
 
   MatrizBit m1, m2, res;
-  bool exito_lectura;
 
-  if (strcmp(argv[1], "NOT") == 0 ||strcmp(argv[1], "TRS") == 0) {
+  if (!strcmp(argv[1], "NOT") || !strcmp(argv[1], "TRS")) {
     if (argc == 3)
       Leer(argv[2], m1);
     else
       Leer(cin, m1);
   }
-  else if (strcmp(argv[1], "AND") == 0 ||strcmp(argv[1], "OR") == 0) {
+  else if (!strcmp(argv[1], "AND")||strcmp(argv[1], "OR")) {
     if (argc == 3) {
       Leer(argv[2], m2);
       Leer(cin, m1);
@@ -36,13 +35,13 @@ int main(int argc, char* argv[]) {
   cout << "m1: " << endl;
   Escribir(cout, m1);
 
-  if (strcmp(argv[1], "NOT") == 0 )
+  if (!strcmp(argv[1], "NOT"))
     Not(res, m1);
-  else if (strcmp(argv[1], "TRS") == 0)
+  else if (!strcmp(argv[1], "TRS"))
     Traspuesta(res, m1);
-  else if (strcmp(argv[1], "AND") == 0)
+  else if (!strcmp(argv[1], "AND"))
     And(res, m1, m2);
-  else if (strcmp(argv[1], "OR") == 0)
+  else if (!strcmp(argv[1], "OR"))
     Or(res, m1, m2);
 
 
